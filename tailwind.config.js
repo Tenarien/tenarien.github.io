5d79cd1f22b8c17741236fb68b85fe188a1cfdc7
Tenarien <mamateki7@gmail.com>
2024-08-14 18:13:03 +0100

    Created boilerplate files to use tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'custom-white': '0 4px 6px rgba(255, 255, 255, 0.5)',
+      },
+    },
+  },
+  plugins: [],
+}
+
