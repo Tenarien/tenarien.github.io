bfeebd22ae0e89cb8101a115d078d86bea2679c9
Tenarien <mamateki7@gmail.com>
2024-08-15 20:45:21 +0100

    Changed exports for optimisation

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./**/*.{html,js}"],
+  content: [
+      "./**/*.html",
+      "./assets/*.js",
+  ],
   theme: {
     extend: {
       boxShadow: {
