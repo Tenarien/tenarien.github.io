1635e3025e6f92b3f108e512f6fec1e888e4445c
Tenarien <mamateki7@gmail.com>
2024-08-21 00:24:38 +0100

    Changed font for the website to roboto, and added those extensions along with some shadows to tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,6 +8,13 @@ module.exports = {
     extend: {
       boxShadow: {
         'custom-white': '0 4px 6px rgba(255, 255, 255, 0.5)',
+        'custom': '0 6px 6px rgba(0, 0, 0, 0.5)',
+        'custom-side': '6px 0px 10px rgba(0, 0, 0, 0.5)',
+        'custom-less': '0 6px 6px rgba(0, 0, 0, 0.25)',
+        'custom-more': '0 6px 6px rgba(0, 0, 0, 0.75)',
+      },
+      fontFamily: {
+        roboto: ['Roboto', 'sans-serif'],
       },
     },
   },
